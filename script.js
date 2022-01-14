09fa20a3241a35c587d47edd327b9c009a169c26
Arif khan <arifkhanshanto123@gmail.com>
2022-01-14 19:09:29 +0600

    Clock Created Using HTML CSS Javascript and first initialized

---
+++ script.js
@@ -0,0 +1,30 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+
+  var hours = currentDate.getHours();
+  if (hours > 12) {
+    var ampm = 'PM';
+  }else{
+    var ampm = 'AM';
+  }
+  document.getElementById("ampm").innerHTML = ampm; 
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
